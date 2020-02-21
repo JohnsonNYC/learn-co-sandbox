2a5c6bbcc0557662a17f4f173e208fc0be01e23e
Johnson Kow <jkow95@gmail.com>
2020-02-21 04:57:36 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,9 @@
+def greeting 
+  (puts "Whats up you fat bitch")
+end 
+
+def say_greeting_five_times
+  5.times {greeting}
+end
+
+say_greeting_five_times
---
+++ methods.rb
@@ -0,0 +1,5 @@
+def greeting_a_person(name, language)
+  puts "Hello #{name}, we hear you are a great #{language} programmer"
+end
+
+greeting_a_person("Carl", language)
